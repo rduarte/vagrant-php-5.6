19e087bae05c4853a0ebfbe1609e92c3406753ae
Ricardo Duarte <ricardo@ricardoduarte.com>
2018-02-28 23:57:24 -0300

    Versão compativel com o Tecmedia Manager

--- Vagrantfile
+++ Vagrantfile
@@ -5,10 +5,11 @@
 VAGRANTFILE_API_VERSION = "2"
 
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
-    config.vm.box = "ubuntu/trusty64"
+    config.vm.box = "ubuntu/trusty32"
 
     config.vm.network :private_network, ip: "192.168.10.56"
     config.vm.network :forwarded_port, guest: 80, host: 80
+    config.vm.network :forwarded_port, guest: 3306, host: 3306
     config.vm.network :forwarded_port, guest: 10081, host: 10081
 
     config.vm.provision :shell, :path => "install.sh"
@@ -23,5 +24,5 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
     # the path on the host to the actual folder. The second argument is
     # the path on the guest to mount the folder. And the optional third
     # argument is a set of non-required options.
-    config.vm.synced_folder "D:/scripts/php/", "/var/www/php"
+    config.vm.synced_folder "/Users/ricardo/git/", "/var/www/php"
 end
--- install.sh
+++ install.sh
@@ -21,15 +21,6 @@ sudo apt-get update
 echo "--- Installing PHP-specific packages ---"
 sudo apt-get install -y php5 apache2 libapache2-mod-php5 php5-curl php5-gd php5-mcrypt php5-intl mysql-server-5.5 php5-mysql php5-sqlite git-core
 
-echo "--- Installing and configuring Xdebug ---"
-sudo apt-get install -y php5-xdebug
-
-cat << EOF | sudo tee -a /etc/php5/mods-available/xdebug.ini
-xdebug.scream=1
-xdebug.cli_color=1
-xdebug.show_local_vars=1
-EOF
-
 echo "--- Enabling mod-rewrite ---"
 sudo a2enmod rewrite
 
@@ -49,33 +40,6 @@ sudo mv composer.phar /usr/local/bin/composer
 sudo /bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=1024
 sudo /sbin/mkswap /var/swap.1
 sudo /sbin/swapon /var/swap.1
-
-# Other Suffs
-
-echo "-- Installing IonCube --"
-cd /usr/local
-sudo wget http://downloads3.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz
-sudo tar xzf ioncube_loaders_lin_x86-64.tar.gz
-sudo mkdir -p /opt/sp/php5.6/lib/php/extensions/ioncube/
-sudo cp ioncube/ioncube_loader_lin_5.6.so /opt/sp/php5.6/lib/php/extensions/ioncube/
-sudo bash -c 'echo "zend_extension=/opt/sp/php5.6/lib/php/extensions/ioncube/ioncube_loader_lin_5.6.so" > /etc/php5/apache2/conf.d/0-ioncube.ini'
-
-echo "-- Installing Z-Ray for Apache --"
-sudo -u
-cd /opt
-sudo wget http://downloads.zend.com/zray/0112/zray-php-102775-php5.6.15-linux-debian7-amd64.tar.gz
-sudo tar xzf zray-php-102775-php5.6.15-linux-debian7-amd64.tar.gz -C /opt
-mv /opt/zray-php-102775-php5.6.15-linux-debian7-amd64/zray /opt/zray
-cp /opt/zray/zray-ui.conf /etc/apache2/sites-available
-a2ensite zray-ui.conf
-ln -sf /opt/zray/zray.ini /etc/php5/apache2/conf.d/zray.ini
-ln -sf /opt/zray/zray.ini /etc/php5/cli/conf.d/zray.ini
-
-# Note:  The exact location of the extensions may vary depending on the specific distro you're installing on.
-ln -sf /opt/zray/lib/zray.so /usr/lib/php5/20131226/zray.so # Debian 8
-#ln -sf /opt/zray/lib/zray.so /usr/lib/php5/20121212/zray.so # Ubuntu 14.04
-
-chown -R www-data:www-data /opt/zray
 
 echo "--- Restarting Apache ---"
 sudo service apache2 restart
